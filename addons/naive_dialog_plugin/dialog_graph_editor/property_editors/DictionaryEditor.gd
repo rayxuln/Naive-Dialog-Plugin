d65d30107efe0af94e0bd8417f2291a2d9525c87
Raiix <imbosslx@sina.com>
2021-11-17 21:51:04 +0800

    make DictionaryEditor recurable.

--- addons/naive_dialog_plugin/dialog_graph_editor/property_editors/DictionaryEditor.gd
+++ addons/naive_dialog_plugin/dialog_graph_editor/property_editors/DictionaryEditor.gd
@@ -17,19 +17,35 @@ var item_popup_menu:PopupMenu
 var item_popup_menu_remove_name := tr('Remove')
 enum ItemPopupMenuId {
 	Remove = 0,
+	TypeCanditateStart = 1000,
 }
 var item_popup_menu_item = null
+var type_canditate_map := {}
 
 func _ready() -> void:
+	add_type_canditate(TYPE_STRING, 'String')
+	add_type_canditate(TYPE_BOOL, 'Bool')
+	add_type_canditate(TYPE_INT, 'Int')
+	add_type_canditate(TYPE_REAL, 'Float')
+	add_type_canditate(TYPE_ARRAY, 'Array')
+	add_type_canditate(TYPE_DICTIONARY, 'Dictionary')
 	create_item_popup_menu()
 #----- Methods -----
+func add_type_canditate(type:int, type_name:String):
+	type_canditate_map[type] = {'name': type_name, 'id': ItemPopupMenuId.TypeCanditateStart + type}
+
 func create_item_popup_menu():
 	item_popup_menu = PopupMenu.new()
+		# add supported type
+	for type in type_canditate_map:
+		item_popup_menu.add_item(type_canditate_map[type].name, type_canditate_map[type].id)
+	
+	item_popup_menu.add_separator()
 	item_popup_menu.add_item(item_popup_menu_remove_name, ItemPopupMenuId.Remove)
 	item_popup_menu.connect('id_pressed', self, '_on_item_popup_menu_id_pressed')
 	add_child(item_popup_menu)
 
-func create_map_item():
+func create_map_item(v):
 	var item = PanelContainer.new()
 	item.size_flags_horizontal = SIZE_EXPAND_FILL
 	item.size_flags_vertical = SIZE_EXPAND_FILL
@@ -43,6 +59,7 @@ func create_map_item():
 	item.add_child(hbox1)
 	
 	var menu_button = Button.new()
+	menu_button.size_flags_vertical = 0
 	menu_button.flat = true
 	menu_button.text = '◎'
 	hbox1.add_child(menu_button)
@@ -51,7 +68,7 @@ func create_map_item():
 	var property_value_pair = PropertyValuePairPrefab.instance()
 	hbox1.add_child(property_value_pair)
 	var pe_def = PropertyEditorDef.new()
-	var editor = pe_def.get_property_editor_instance_by_type(get_type_hint())
+	var editor = pe_def.get_property_editor_instance_by_type(typeof(v))
 	item.set_meta('pair', property_value_pair)
 	item.set_meta('editor', editor)
 	
@@ -65,7 +82,7 @@ func update_map():
 	clear_map_item()
 	for k in map.keys():
 		var v = map[k]
-		var item = create_map_item()
+		var item = create_map_item(v)
 		list_container.add_child(item)
 		item.set_meta('key', k)
 		var menu_button:Button = item.get_meta('menu_button')
@@ -78,11 +95,11 @@ func update_map():
 		
 		var editor = item.get_meta('editor')
 		if editor:
-			editor.set_value(v)
 			editor.connect('value_changed', self, '_on_item_value_changed', [item])
 			pair.set_editor(editor)
+			editor.set_value(v)
 		else:
-			printerr('Unsupport array type: %s' % get_type_hint())
+			printerr('Unsupport map type: %s' % typeof(v))
 
 func add_map_item_at(id):
 	map[id] = ''
@@ -105,17 +122,17 @@ func get_layout_type():
 func update_size():
 	rect_size = Vector2.ZERO
 
-func get_type_hint():
-	if property_def != null:
-		if property_def.has('type_hint'):
-			if typeof(property_def.type_hint) == typeof(TYPE_INT):
-				return property_def.type_hint
-	return TYPE_STRING
+func get_empty_value_by_type(type:int):
+	var def := DialogGraphDataDef.new()
+	return def.get_empty_value_by_type(type)
 #----- Signals -----
 func _on_item_value_changed(v, item):
 	map[item.get_meta('key')] = v
 
 func _on_AddButton_pressed() -> void:
+	if map.has(key_line_edit.text):
+		printerr('key: \'%s\' is already exist!' % [key_line_edit.text])
+		return
 	add_map_item_at(key_line_edit.text)
 
 func _on_item_popup_menu_id_pressed(id:int):
@@ -124,6 +141,12 @@ func _on_item_popup_menu_id_pressed(id:int):
 	match id:
 		ItemPopupMenuId.Remove:
 			remove_map_item_at(item_popup_menu_item.get_meta('key'))
+	if id >= ItemPopupMenuId.TypeCanditateStart:
+		var new_type = id - ItemPopupMenuId.TypeCanditateStart
+		var val = get_empty_value_by_type(new_type)
+		map[item_popup_menu_item.get_meta('key')] = val
+		update_map()
+	
 	item_popup_menu_item = null
 
 func _on_item_menu_button_pressed(item):
